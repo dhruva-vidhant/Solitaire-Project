ca2eb07f456610b593161f88bb3a64c0141baa91
dhruva-vidhant <66923429+dhruva-vidhant@users.noreply.github.com>
2025-05-13 21:17:25 +0000

    added to card class

--- Card.java
+++ Card.java
@@ -1,3 +1,13 @@
 public class Card {
-    
+    public enum Suit {
+        SPADES(true), CLUBS(true), HEARTS(false), DIAMONDS(false);
+
+        public final boolean isBlack;
+
+        Suit(boolean color) {
+            isBlack = color;
+        }
+    }
+    private Suit suit;
+    private int number;
 }
