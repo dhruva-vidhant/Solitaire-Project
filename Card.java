739ac0efd11292c802cc7309d31d5e6f3240ed5f
dhruva-vidhant <66923429+dhruva-vidhant@users.noreply.github.com>
2025-05-14 04:19:00 +0000

    Finished card and created deck class

--- Card.java
+++ Card.java
@@ -10,4 +10,12 @@ public class Card {
     }
     private Suit suit;
     private int number;
+
+    public Card(Suit suit, int number) {
+        this.suit = suit;
+        this.number = number;
+    }
+
+    public Suit getSuit() {return suit;}
+    public int getNumber() {return number;}
 }
