de42488c2ffd5275cc84f2934abfc7bfc5e9af36
jeffreyThe3155 <jjl28282@gmail.com>
2025-05-19 13:58:12 -0700

    Update Pile.java
    added comments on the variables in different methods.

--- Pile.java
+++ Pile.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Pile extends ArrayList<Card> {
     private int firstVisible;
 
+    //"num" is the number of cards that a pile will be initialized with
     public Pile(RandomAccessArray<Card> allCards, int num) {
         for(int i = 0; i < num; i++) {
             add(allCards.removeRandom());
@@ -17,13 +18,15 @@ public class Pile extends ArrayList<Card> {
         addAll(newCards);
     }
 
+    //"startIdx" is the index of the first card that is removed out of the pile that this object represents
     public List<Card> removeCards(int startIdx) {
         List<Card> result = new ArrayList<>(subList(startIdx, size()));
         removeRange(startIdx, size());
         firstVisible = startIdx-1;
         return result;
     }
-    
+
+    //"idx" means the index of a card in a pile
     public String display(int idx) {
         if(idx >= size()) return "  ";
         if(idx < firstVisible) return "[]";
