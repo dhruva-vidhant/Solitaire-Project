b54fcb3960ac86e746d8e4b571d8aa4b1a913e56
dhruva-vidhant <66923429+dhruva-vidhant@users.noreply.github.com>
2025-05-23 13:33:44 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -6,7 +6,8 @@ public class Game {
     private Deque<Card> deck = new ArrayDeque<>();
     private Deque<Card> wastePile = new ArrayDeque<>();
     private FoundationPile[] foundationPiles = new FoundationPile[4];
-    private Pile[] piles = new Pile[7];
+      private static int numCols = 5; 
+    private Pile[] piles = new Pile[numCols];
 
     public Game() {
         Card.Suit[] suits = Card.Suit.values();
@@ -14,7 +15,7 @@ public class Game {
             foundationPiles[i] = new FoundationPile(suits[i]);
         }
         var allCards = allCards();
-        for(int i = 1; i <= 7; i++) {
+        for(int i = 1; i <= numCols; i++) {
             piles[i-1] = new Pile(allCards, i);
         }
         Card c;
@@ -36,6 +37,7 @@ public class Game {
     }
 
     private boolean isValid(Card firstCard, Card lastCard) {
+        if(firstCard==null) return lastCard.getNumber() == 13;
         return (firstCard.getNumber() == lastCard.getNumber()-1) && (firstCard.isBlack() != lastCard.isBlack());
     }
 
@@ -49,7 +51,7 @@ public class Game {
     //assumes pileIdx is valid and is not empty
     public boolean tableauToFoundation(int pileIdx, int foundationIdx) {
         Pile p = piles[pileIdx];
-        boolean result = foundationPiles[foundationIdx].addCard(p.getLast());
+        boolean result = foundationPiles[foundationIdx].addCard(p.removeLast());
         if(result) p.remove(p.size()-1);
         return result;
     }
@@ -64,11 +66,18 @@ public class Game {
         return false;
     }
 
+    public boolean isWon() {
+        for(var p : foundationPiles) {
+            if(!p.isDone()) return false;
+        }
+        return true;
+    }
+
     //assumes valid fromIdx and piles
     public boolean moveCards(int fromPile, int fromIdx, int toPile) {
         Pile from = piles[fromPile];
         Pile to = piles[toPile];
-        if(isValid(from.get(fromIdx), to.get(to.size()-1))) {
+        if(isValid(from.isEmpty() ? null : from.get(fromIdx), to.get(to.size()-1)) && fromIdx < from.size()) {
             piles[toPile].add(piles[fromPile].removeCards(fromIdx));
             return true;
         }
@@ -76,17 +85,22 @@ public class Game {
     }
 
     public void printState() {
-        System.out.print(deck.isEmpty() ? "  " : "[]");
-        System.out.print("  ");
-        System.out.print(wastePile.isEmpty() ? "  " : wastePile.peek());
+        System.out.print(deck.isEmpty() ? "  " : "[]"); //Grab deck
+        System.out.print("   ");
+        System.out.print(wastePile.isEmpty() ? "   " : wastePile.peek()); //Grab deck left over
         System.out.print("    ");
-        System.out.println("♠:♣:♥:♦:");
-        System.out.print("          ");
-        for(FoundationPile fp : foundationPiles) System.out.printf("%.2s", fp.peek() != null ? fp.peek() : "[]");
+        System.out.println("♠:♣:" + "\u001B[31m♥" + "\u001B[30m:" + "\u001B[31m♦" + "\u001B[30m:");
+        System.out.print("            ");
+        for(FoundationPile fp : foundationPiles) System.out.printf("%s", fp.peek() != null ? fp.peek() : "[]"); //Suit decks
         System.out.println();
-        for(int i = 0; i < 7; i++) {
+        System.out.println("  1  2  3  4  5"); //Col nums
+        int largestLen = piles[0].size();
+        for(int i = 0; i < numCols; i++)
+            if(piles[i].size() > largestLen) largestLen = piles[i].size();
+        for(int i = 0; i < largestLen; i++) { //Print Cols
+            System.out.print(i + 1 + " ");
             for(int j = 0; j < piles.length; j++) {
-                System.out.printf("%.2s", piles[j].display(i));
+                System.out.printf("%s", piles[j].display(i));
             }
             System.out.println();
         }
